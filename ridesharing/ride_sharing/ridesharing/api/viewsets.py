3e133b5081482f726ab02b6146bef2f58677cc0e
Alexandra <anda.vatamanu@gmail.com>
2019-03-17 12:07:27 +0200

    Add ride listing

--- ridesharing/ride_sharing/ridesharing/api/viewsets.py
+++ ridesharing/ride_sharing/ridesharing/api/viewsets.py
@@ -54,6 +54,7 @@ class UserViewSet(viewsets.GenericViewSet):
         user.save()
         return response.Response(status=200, data={"message": "OK"})
 
+
 class RideViewSet(viewsets.GenericViewSet):
     serializer_class = RideSerializer
     queryset = AppUser.objects.all()
@@ -123,3 +124,16 @@ class RideViewSet(viewsets.GenericViewSet):
             return response.Response(status=200, data={'message': "OK"})
 
         return response.Response(status=404, data={'error': 'No more free seats :('})
+
+    @detail_route(methods=['get'])
+    def list_rides_history(self, request, pk, **kwargs):
+        try:
+            user = AppUser.objects.get(pk=pk)
+        except AppUser.DoesNotExist:
+            return response.Response(status=404, data={'error': 'This user does not exist!'})
+        
+        print(pk)
+        all_entries = Ride.objects.filter(passengers__ids__contains=pk)
+        print(all_entries)
+        serializer = self.get_serializer(all_entries, many=True)
+        return response.Response(serializer.data)
