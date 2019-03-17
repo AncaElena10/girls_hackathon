16e0dc625a9c5ee4b7121165bd57b6a951ae55f7
alina.manu <alina.manu13@yahoo.com>
2019-03-17 03:04:00 +0200

    Charts

--- Angular/src/app/components/passenger/passenger.component.ts
+++ Angular/src/app/components/passenger/passenger.component.ts
@@ -6,14 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./passenger.component.css']
 })
 export class PassengerComponent implements OnInit {
-  chart: Object;
+  piechart;
+  linechart;
+  barchart;
 
   ngOnInit() {
 
   }
 
   constructor() {
-    this.chart = {
+    this.piechart = {
       title: 'Pie Chart',
       type: 'PieChart',
       columnNames: ['Task', 'Hours per Day'],
@@ -21,10 +23,39 @@ export class PassengerComponent implements OnInit {
         ['Work', 11],
         ['Eat', 2],
         ['Commute', 2],
-        ['Watch TV', 2],
         ['Sleep', 7]
       ],
       roles: []
+    };
+    this.linechart = {
+      title: 'Styled Line Chart',
+      type: 'LineChart',
+      columnNames: ['Element', 'Density'],
+      roles: [
+        { type: 'number', role: 'interval' },
+        { type: 'number', role: 'interval' },
+        { type: 'string', role: 'annotation' },
+        { type: 'string', role: 'annotationText' },
+        { type: 'boolean', role: 'certainty' }
+      ],
+      data: [
+        ['April', 1000],
+        ['May', 1170],
+        ['June', 660],
+        ['July', 1030]
+      ]
+    };
+    this.barchart = {
+      title: 'Material Bar Chart',
+      type: 'Bar',
+      columnNames: ['Year', 'Sales', 'Expenses', 'Profit'],
+      roles: [],
+      data: [
+        ['2014', 1000, 400, 200],
+        ['2015', 1170, 460, 250],
+        ['2016', 660, 1120, 300],
+        ['2017', 1030, 540, 350]
+      ],
     };
 
   }
